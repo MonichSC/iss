6482b53032af8862e44d1aa4d167cde7f4455e69
Jurek Muszyński <36708070+silgy@users.noreply.github.com>
2020-10-24 14:38:48 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
 var x = new XMLHttpRequest();
 
 
+// --------------------------------------------------------------------------
+// Show wait animation
+// --------------------------------------------------------------------------
+function wait()
+{
+    if ( !document.getElementById("wait") )
+    {
+        let w = document.createElement("div");
+        w.id = "wait";
+        w.className = "wt";     // see silgy.css
+        w.style.display = "block";
+        document.body.appendChild(w);
+    }
+    else
+        document.getElementById("wait").style.display = "block";
+}
+
+
+// --------------------------------------------------------------------------
+// Turn the spinning wheel off
+// --------------------------------------------------------------------------
+function wait_off()
+{
+    document.getElementById("wait").style.display = "none";
+}
+
+
+// --------------------------------------------------------------------------
+// Append a paragraph to the page
+// --------------------------------------------------------------------------
+function p(t)
+{
+    let p = document.createElement("p");
+    if ( t ) p.innerHTML = t;
+    document.body.appendChild(p);
+    return p;
+}
+
+
+// --------------------------------------------------------------------------
+// Run
+// --------------------------------------------------------------------------
 function iss_go()
 {
     let l = document.getElementById("start_level").value;
@@ -10,6 +52,24 @@ function iss_go()
         if ( x.readyState == 4 )
         {
             wait_off();
+
+            if ( x.responseText.length > 0 )
+            {
+                let r = JSON.parse(x.responseText);
+
+                if ( r.code != 0 )  // error -- show message
+                {
+                    p(r.message);
+                }
+                else    // ok
+                {
+                    p("iss_go OK");
+                }
+            }
+            else    // empty
+            {
+                p("Błąd iss_go -- pusta odpowiedź");
+            }
         }
     };
 
