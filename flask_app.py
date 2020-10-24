693527210aed8d12159dc9e436ad1c90c462534e
Jurek Muszyński <36708070+silgy@users.noreply.github.com>
2020-10-24 14:47:28 +0200

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,8 @@
 from flask import Flask, escape, request, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def hello():
-    #    name = request.args.get("name", "World")
+    # name = request.args.get("name", "World")
     return render_template("main.html")
