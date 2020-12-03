adc8dc9dc224d2618215910c84cc76faf5e170fe
Michal Rostocki <michalrostocki@o2.pl>
2020-12-03 21:42:40 +0100

    Add basic fuzzy controller

--- multi_container.py
+++ multi_container.py
@@ -3,7 +3,8 @@ import math
 
 class multi_container:
 
-    def __init__(self, start_height, target_height, max_height, area=2.1, ticks_per_second=100, max_input=0.1, beta=0.035):
+    def __init__(self, start_height, target_height, max_height, area=2.1, ticks_per_second=100, max_input=0.1,
+                 beta=0.035):
         # Maximum amount of water in a container
         self.max_height = max_height
         # Target water amount
@@ -25,11 +26,14 @@ class multi_container:
         # Start valves as closed
         self.input_valve_status.append(0)
         self.output_valve_status.append(0)
+        # Errors
+        self.errors = []
 
     def tick(self):
-        # Control
-        self.input_valve_status.append(1)
-        self.output_valve_status.append(1)
+        # Error calculation
+        self.errors.append(self.target_height - self.current_height[-1])
+        # Control values
+        self.control()
         # Simulation
         input_per_second = self.max_input * self.input_valve_status[-1] / self.area
         output_per_second = self.output_valve_status[-1] * self.beta * math.sqrt(self.current_height[-1])
@@ -44,12 +48,45 @@ class multi_container:
             new_height = self.max_height
         self.current_height.append(new_height)
 
+    def control(self):
+        # For now input valve is always on
+        self.input_valve_status.append(1)
+
+        if len(self.errors) <= 2:
+            self.output_valve_status.append(self.output_valve_status[-1])
+            return
+
+        # Reasons
+        reasons_raw = [
+            # Reason 0: error is falling
+            abs(self.errors[-1]) - abs(self.errors[-2]),
+            # Reason 1: error is raising
+            abs(self.errors[-2]) - abs(self.errors[-1]),
+            # Reason 2: error is positive
+            self.errors[-1],
+            # Reason 3: error is negative
+            -self.errors[-1],
+        ]
+
+        reasons = []
+        for r in reasons_raw:
+            reasons.append(max(min(r, 1), 0))
+
+        output_change = 0
+        # Rule 1: IF error is positive (R2) => output_change is going up
+        output_change += -reasons[2]
+        # Rule 1: IF error is negative (R3) => output_change is going down
+        output_change += reasons[3]
+
+        new_output = min(max(self.output_valve_status[-1] + output_change, 0), 1)
+        self.output_valve_status.append(new_output)
+
     def get_heights(self):
         return self.current_height
 
 
-def demo(start_height, target_height=5, max_height=10, area=2, ticks_per_second=1000,
-         max_input=0.1, beta=0.035, sim_time=60):
+def demo(start_height, target_height=4, max_height=10, area=2, ticks_per_second=1000,
+         max_input=0.1, beta=0.035, sim_time=600):
     """
     Launches simulation of the multi container
     Parameters
@@ -76,3 +113,9 @@ def demo(start_height, target_height=5, max_height=10, area=2, ticks_per_second=
     for i in range(sim_time * ticks_per_second):
         obj.tick()
     return obj.get_heights()
+
+
+if __name__ == '__main__':
+    print("START")
+    demo(6)
+    print("DONE")
