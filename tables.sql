90382a40d8e20f6a75b527dcea30c876bcd7a3b7
Jurek <jmuszynski@man.poznan.pl>
2021-02-10 20:51:06 +0100

    db stub

--- tables.sql
+++ tables.sql
@@ -1,5 +1,11 @@
 create database iss;
 
+create table simulations (
+    id int auto_increment primary key,
+    created datetime,
+    komentarz varchar(255)
+);
+
 create table results (
     sim_id integer,
     elapsed float,
