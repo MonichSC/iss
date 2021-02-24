c831458a8ed91fd0f03964a7833d71a692c46cf5
Jurek <jmuszynski@man.poznan.pl>
2021-02-24 18:22:39 +0100

    PID znormalizowany

--- controllers/pid_controller.py
+++ controllers/pid_controller.py
@@ -15,13 +15,13 @@ class PidController:
         self.pid = PID(pid_params["temperature"]["p"], pid_params["temperature"]["i"], pid_params["temperature"]["d"], setpoint = target_temp)
         self.pid.setpoint = target_temp
         self.pid.sample_time = 1
-        self.pid.output_limits = (0, self.max_heater_power) 
+        self.pid.output_limits = (0, 1) 
 
 
 
     def tick(self, sim_object):
 
-        new_power = self.pid(sim_object.temperature[-1])
+        new_power_factor = self.pid(sim_object.temperature[-1])
 
         sim_object.heater_status.append(1)    # always on
-        sim_object.heater_power.append(new_power)
+        sim_object.heater_power.append(new_power_factor * self.max_heater_power)
